57529a72daad012cf71b1362e70d841302d24273
mousehacks <77026371+mousehacks@users.noreply.github.com>
2021-02-07 20:57:07 +0100

    final commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fName = "Nadine";
+const lName = "Krause";
+const luckyNumber = "13";
+
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello {fName} {lName}!
+    </h1>
+    <p>Your lucky Number is {luckyNumber}.</p>
+  </div>,
+  document.getElementById("root")
+);
